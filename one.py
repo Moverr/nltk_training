d6cb8af5d0add46113bc04e2efd9e3616bbf6155
Muyinda Rogers <moverr@gmail.com>
2019-11-28 15:24:58 +0300

    :Debugging :

--- one.py
+++ one.py
@@ -3,9 +3,7 @@ from  nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 example_sentence = "This is an example showing off stop word filtration."
-
 stop_words = set(stopwords.words("english"))
-
 print("stop Words \n {} ".format(stop_words))
 
 words = word_tokenize(example_sentence)
