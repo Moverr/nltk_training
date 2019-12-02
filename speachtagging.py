43b55d85b3bee0c876662e1d74d98256d1b3d4cf
Muyinda Rogers <moverr@gmail.com>
2019-12-02 11:30:05 +0300

    Train PunksentenceTOkenizer

--- speachtagging.py
+++ speachtagging.py
@@ -1,6 +1,11 @@
 import nltk
 from nltk.corpus import state_union
 from nltk.tokenize import PunktSentenceTokenizer
+# Un supervised machine learning tokenizer , it comes supervised but you can re-train it for other scneanrois
 
+train_text = state_union.raw("2005-GWBush.txt")
+sample_text = state_union.raw("2006-GWBush.txt")
 
-print(state_union)
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
+
+print(sample_text)
