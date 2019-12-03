758d74219f4cd6935c17ed874f454fe6abd4420f
Muyinda Rogers <moverr@gmail.com>
2019-12-03 16:00:27 +0300

    getting a list of all words :

--- textclassification.py
+++ textclassification.py
@@ -6,7 +6,16 @@ from nltk.corpus import movie_reviews
 
 # print(movie_reviews())
 
-documents = []
-for category  in movie_reviews.categories():
-    for fileid in movie_reviews.fileids(category):
-        documents.append()
+documents = [(list(movie_reviews.words(fileid)),category)  
+            for category in movie_reviews.categories() 
+            for fileid in movie_reviews.fileids(category)]
+
+
+random.shuffle(documents)
+print(documents[1])
+
+
+all_words = []
+for w in movie_reviews.words():
+    all_words.append(w.lower())
+ 
