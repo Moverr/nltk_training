4a282ad428d84f1352c0e4d25c5b6bb32e8e011e
Muyinda Rogers <moverr@gmail.com>
2019-12-03 11:55:56 +0300

    Lametzing is bettwer than Stemming

--- Lametizing.py
+++ Lametizing.py
@@ -8,3 +8,15 @@ print(lemmatizer.lemmatize("geese"))
 print(lemmatizer.lemmatize("rocks"))
 print(lemmatizer.lemmatize("python"))
 print(lemmatizer.lemmatize("mountains"))
+
+
+
+print(lemmatizer.lemmatize("better",pos='a'))
+
+print(lemmatizer.lemmatize("best",pos='a'))
+print(lemmatizer.lemmatize("run",pos='v'))
+print(lemmatizer.lemmatize("run",pos='v'))
+
+
+# Lametizing is more powerful than stemming
+
