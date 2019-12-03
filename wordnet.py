da92a058bde967b5ac851941976530930fb2fe72
Muyinda Rogers <moverr@gmail.com>
2019-12-03 13:36:43 +0300

    :Debugging

--- wordnet.py
+++ wordnet.py
@@ -13,3 +13,13 @@ print(syns[0].definition())
 
 # Examples 
 print(syns[0].examples())
+
+
+synonyms = []
+antonyms = []
+
+for syn  in wordnet.synsets("good"):
+    for l in syn.lemmas():
+        synonyms.append(l.name())
+        if l.antonyms():
+            antonyms.append(l.antonyms()[0].name)
