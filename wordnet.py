cb8c05413b6299a62be9167af8cf9f92c9dfc8de
Muyinda Rogers <moverr@gmail.com>
2019-12-03 12:54:52 +0300

    :Debugging :

--- wordnet.py
+++ wordnet.py
@@ -2,3 +2,12 @@ from nltk.corpus import wordnet
 
 syns = wordnet.synsets("program")
 print(syns)
+
+# synset 
+print(syns[0].name)
+# Just the word 
+print(syns[0].lemmas()[0].name())
+
+# Definition 
+print(syns[0].definition())
+
