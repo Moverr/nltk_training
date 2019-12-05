59fa0c7b96ac6c5a1096626580a69de224f8992f
Muyinda Rogers <moverr@gmail.com>
2019-12-05 16:21:24 +0300

    :Debugging :

--- custoombayes.py
+++ custoombayes.py
@@ -3,19 +3,13 @@ import random
 from nltk.corpus import movie_reviews
 
 
-documents = [(list(movie_reviews.words(fileid)),category)
-             for category in movie_reviews.categories()
-             for fileid in movie_reviews.fileids(category)
-        ]
+# documents = [(list(movie_reviews.words(fileid)),category)
+#              for category in movie_reviews.categories()
+#              for fileid in movie_reviews.fileids(category)
+#         ]
+
 
 political_content = {}
-political_content["true"] = [
-    "Uganda Politician Bobiwine has been sentenced to 19 years in prison, for fraund",
-    "Uganda Politician Bobiwine has been elected the president oof uganda in 2019",
-    "President Museveni has  stepped aside to let bobiwine be the next president of Uganda",
-    "President Museveni has been found guilty of fraud, he has been sentenced to jail 39 years in prison " 
-]
-
 political_content["false"] = [
     "Uganda Politician Bobiwine has been sentenced to 19 years in prison, for fraund",
     "Uganda Politician Bobiwine has been elected the president oof uganda in 2019",
@@ -23,9 +17,16 @@ political_content["false"] = [
     "President Museveni has been found guilty of fraud, he has been sentenced to jail 39 years in prison " 
 ]
 
+political_content["true"] = [
+    "Uganda  president Museveni is going to stand for the 2021 elections",
+    "Uganda  politician bobiwine intends to stand for presidenet in 2021",
+    "Uganda president museveni has launched a campain against corruption"  
+]
 
 
-random.shuffle(documents)
+
+print(political_content)
+# random.shuffle(documents)
 
 
-print(documents[0])
+# print(documents[0])
