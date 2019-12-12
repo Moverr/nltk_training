e690aa795b6a8215d5dba1c902356d1f837bb056
Muyinda Rogers <moverr@gmail.com>
2019-12-12 11:13:22 +0300

    Extending data into different aspects :

--- custoombayes.py
+++ custoombayes.py
@@ -5,7 +5,11 @@ from nltk.classify.scikitlearn import SklearnClassifier
 from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
 from sklearn.linear_model import  LogisticRegression, SGDClassifier
 from sklearn.svm import SVC, LinearSVC,NuSVC
+import json
 
+
+with open('data.txt') as json_file:
+    data = json.load(json_file)
  
 political_content = {}
 political_content["false"] = [
