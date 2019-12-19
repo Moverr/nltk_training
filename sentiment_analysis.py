e56cfdd677b3284414008c20e038a9ef44f2222a
Muyinda Rogers <moverr@gmail.com>
2019-12-19 13:02:55 +0300

    :Debugging :

---
+++ sentiment_analysis.py
@@ -0,0 +1,10 @@
+import numpy as np
+import re
+import nltk
+from sklearn.datasets import load_files
+nltk.download('stopwords')
+import pickle
+from nltk.corpus import stopwords
+
+
+print(" INteresting")
